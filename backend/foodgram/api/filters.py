57a08dd79185369f66da15b3b7111b003eaca1aa
Виктория Мальцева <sidyakova24@gmail.com>
2022-09-01 00:16:52 +0500

    change in docker-compose9

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,24 +1,56 @@
 import django_filters
 from rest_framework import filters
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(django_filters.FilterSet):
-    tags = django_filters.CharFilter(
-        field_name='tags',
-        lookup_expr='slug'
+    tags = django_filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name='tags__slug',
+        to_field_name='slug',
     )
+    is_favorited = django_filters.BooleanFilter(
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = django_filters.BooleanFilter(
+        method='get_is_in_shopping_cart'
+    )
+
+    def get_is_favorited(self, queryset, value):
+        if value:
+            return queryset.filter(
+                favorited__user=self.request.user
+            )
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, value):
+        if value:
+            return queryset.filter(
+                shopping_cart__user=self.request.user
+            )
+        return queryset
 
     class Meta:
         model = Recipe
-        fields = (
-            'is_favorited',
-            'author',
-            'is_in_shopping_cart',
-            'tags'
-        )
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart',)
 
 
 class IngredientFilter(filters.SearchFilter):
     search_param = 'name'
+
+
+# class RecipeFilter(django_filters.FilterSet):
+#     tags = django_filters.CharFilter(
+#         field_name='tags',
+#         lookup_expr='slug'
+#     )
+#
+#     class Meta:
+#         model = Recipe
+#         fields = (
+#             'is_favorited',
+#             'author',
+#             'is_in_shopping_cart',
+#             'tags'
+#         )
